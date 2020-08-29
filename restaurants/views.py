5d3c3191f4b6081c562ef17850d6206c12c9e300
mmrg98 <mariam.elgaria98@gmail.com>
2020-08-30 00:12:57 +0300

    task 3 is done

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,9 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+    "my_list":[{"restaurant_name":"Mac", "food_type":"Burger"},
+    {"restaurant_name":"Pizza Hut","food_type":"Pizza"},
+    {"restaurant_name":"Steak House","food_type":"Steak"}]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +16,7 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    "my_object":{"restaurant_name":"Steak House","food_type":"Steak"}
 
     }
     return render(request, 'detail.html', context)
